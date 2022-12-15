017ce0de022defe47ad16d0088c52b92402aaeb0
Essteel <siannon.steel@gmail.com>
2022-12-15 14:14:02 +1030

    Completed CR routes for Entries

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const categories = ['Food', 'Coding', 'Work', 'Other']
 
-const entries = [
-    {category: 'Food', content: 'Hello!'},
-    {category: 'Coding', content: 'Express is cool!'},
-    {category: 'Work', content: 'Another day at the office.'}
-]
+// const entries = [
+//     {category: 'Food', content: 'Hello!'},
+//     {category: 'Coding', content: 'Express is cool!'},
+//     {category: 'Work', content: 'Another day at the office.'}
+// ]
+
+// Connect to MongoDB via Mongoose
+mongoose.connect('mongodb+srv://ssteel:Th15isAdminL3af@ca-cluster.kxa8xzn.mongodb.net/journal')
+    .then((m) => m.connection._readyState === 1 ? console.log('Mongoose connected!') : console.log('Mongoose failed to connect'))
+    .catch((err) => console.log(err))
+
+    // Create a Mongoose schema to define the structure of a model
+const entrySchema = new mongoose.Schema({
+    category: {type: String, required: true},
+    content: {type: String, required: true}
+})
+
+//Create a Mongoose model based on the schema
+const EntryModel = mongoose.model('Entry', entrySchema)
 
 const app = express()
 const port = 4001
@@ -17,26 +32,37 @@ app.get('/', (request, response) => response.send({ info: 'Journal API' }))
 
 app.get('/categories', (req, res) => res.send(categories))
 
-app.get('/entries', (req, res) => res.send(entries))
+app.get('/entries', async (req, res) => res.send(await EntryModel.find()))
 
-app.get('/entries/:id', (req, res) => {
-    const entry = entries[req.params.id]
-    if (entry) {
-        res.send(entry)
-    } else {
-        res.status(404).send({error: 'Entry not found'})
+app.get('/entries/:id', async (req, res) => {
+    try {
+        const entry = await EntryModel.findById(req.params.id)
+        if (entry) {
+            res.send(entry)
+        } else {
+            res.status(404).send({error: 'Entry not found'})
+        }
+    }   
+    catch (err) {
+        res.status(500).send({error: err.message})
     }
 })
 
-app.post('/entries', (req, res) => {
-    // 1. Create a new entry object with values passed in from the request
-    const { category, content } = req.body
-    // Validation and sanitization
-    const newEntry = {category, content}
-    // 2. Push new entry to the entries array
-    entries.push(newEntry)
-    // 3. Send the new entry with 201 status code
-    res.status(201).send(newEntry)
+app.post('/entries', async (req, res) => {
+    try {
+        // 1. Create a new entry object with values passed in from the request
+        const { category, content } = req.body
+        // Validation and sanitization comes from Model
+        const newEntry = {category, content}
+        // 2. Push new entry to the entries array
+        // entries.push(newEntry)
+        const insertedEntry = await EntryModel.create(newEntry)
+        // 3. Send the new entry with 201 status code
+        res.status(201).send(insertedEntry)
+    }
+    catch (err) {
+        res.status(500).send({error: err.message})
+    }
 })
 
 app.listen(port, () => console.log(`App running at http://localhost:${port}`))
