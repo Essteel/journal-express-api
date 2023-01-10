2bca2d79d04d06148d8853597a0754669755f40f
Essteel <siannon.steel@gmail.com>
2023-01-10 13:45:29 +1030

    Modularised entry routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { EntryModel, CategoryModel } from './db.js'
+import { CategoryModel } from './db.js'
+import entryRoutes from './routes/entry.js'
 
 const app = express()
 const port = 4001
@@ -8,6 +9,8 @@ app.use(express.json())
 
 // HOME ROUTE
 app.get('/', (request, response) => response.send({ info: 'Journal API' }))
+
+app.use('/entries', entryRoutes)
 
 // ### CATEGORY ROUTES ###
 
@@ -74,77 +77,6 @@ app.post('/categories', async (req, res) => {
         const insertedCategory = await CategoryModel.create(newCategory)
         // 3. Send the new entry with 201 status code
         res.status(201).send(insertedCategory)
-    }
-    catch (err) {
-        res.status(500).send({error: err.message})
-    }
-})
-
-// ### ENTRY ROUTES ###
-
-// Get all entries
-app.get('/entries', async (req, res) => res.send(await EntryModel.find().populate({path: 'category', select: 'name'})))
-
-// Get one entry
-app.get('/entries/:id', async (req, res) => {
-    try {
-        const entry = await EntryModel.findById(req.params.id).populate({ path: 'category', select: 'name '})
-        if (entry) {
-            res.send(entry)
-        } else {
-            res.status(404).send({error: 'Entry not found'})
-        }
-    }   
-    catch (err) {
-        res.status(500).send({error: err.message})
-    }
-})
-
-// Update an entry
-app.put('/entries/:id', async (req, res) => {
-    const { category, content } = req.body
-    const newEntry = { category, content }
-
-    try {
-        const entry = await EntryModel.findByIdAndUpdate(req.params.id, newEntry, { returnDocument: 'after' })
-        if (entry) {
-            res.send(entry)
-        } else {
-            res.status(404).send({error: 'Entry not found'})
-        }
-    }   
-    catch (err) {
-        res.status(500).send({error: err.message})
-    }
-})
-
-// Delete an entry
-app.delete('/entries/:id', async (req, res) => {
-    try {
-        const entry = await EntryModel.findByIdAndDelete(req.params.id)
-        if (entry) {
-            res.sendStatus(204)
-        } else {
-            res.status(404).send({error: 'Entry not found'})
-        }
-    }   
-    catch (err) {
-        res.status(500).send({error: err.message})
-    }
-})
-
-// Add a new entry
-app.post('/entries', async (req, res) => {
-    try {
-        // 1. Create a new entry object with values passed in from the request
-        const { category, content } = req.body
-        // Validation and sanitization comes from Model
-        const newEntry = {category, content}
-        // 2. Push new entry to the entries array
-        // entries.push(newEntry)
-        const insertedEntry = await EntryModel.create(newEntry)
-        // 3. Send the new entry with 201 status code
-        res.status(201).send(insertedEntry)
     }
     catch (err) {
         res.status(500).send({error: err.message})
---
+++ routes/entry.js
@@ -0,0 +1,75 @@
+import express from 'express'
+import { EntryModel } from '../db.js'
+
+const router = express.Router()
+
+// Get all entries
+router.get('/', async (req, res) => res.send(await EntryModel.find().populate({path: 'category', select: 'name'})))
+
+// Get one entry
+router.get('/:id', async (req, res) => {
+    try {
+        const entry = await EntryModel.findById(req.params.id).populate({ path: 'category', select: 'name '})
+        if (entry) {
+            res.send(entry)
+        } else {
+            res.status(404).send({error: 'Entry not found'})
+        }
+    }   
+    catch (err) {
+        res.status(500).send({error: err.message})
+    }
+})
+
+// Update an entry
+router.put('/:id', async (req, res) => {
+    const { category, content } = req.body
+    const newEntry = { category, content }
+
+    try {
+        const entry = await EntryModel.findByIdAndUpdate(req.params.id, newEntry, { returnDocument: 'after' })
+        if (entry) {
+            res.send(entry)
+        } else {
+            res.status(404).send({error: 'Entry not found'})
+        }
+    }   
+    catch (err) {
+        res.status(500).send({error: err.message})
+    }
+})
+
+// Delete an entry
+router.delete('/:id', async (req, res) => {
+    try {
+        const entry = await EntryModel.findByIdAndDelete(req.params.id)
+        if (entry) {
+            res.sendStatus(204)
+        } else {
+            res.status(404).send({error: 'Entry not found'})
+        }
+    }   
+    catch (err) {
+        res.status(500).send({error: err.message})
+    }
+})
+
+// Add a new entry
+router.post('/', async (req, res) => {
+    try {
+        // 1. Create a new entry object with values passed in from the request
+        const { category, content } = req.body
+        // Validation and sanitization comes from Model
+        const newEntry = {category, content}
+        // 2. Push new entry to the entries array
+        // entries.push(newEntry)
+        const insertedEntry = await EntryModel.create(newEntry)
+        // 3. Send the new entry with 201 status code
+        res.status(201).send(insertedEntry)
+    }
+    catch (err) {
+        res.status(500).send({error: err.message})
+    }
+})
+
+export default router
